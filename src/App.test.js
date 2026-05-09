a597440dbe43e2c6db97b8a4086d783f9f77fe61
ROHIT-213 <rs2101396@gmail.com>
2026-05-10 00:10:26 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   //const headingElement = screen.getByText(/TODO LIST/i);
+//   // const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders todo heading', () => {
   render(<App />);
-  //const headingElement = screen.getByText(/TODO LIST/i);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/TODO LIST/i);
+  expect(headingElement).toBeInTheDocument();
 });
