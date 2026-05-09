11716e7846a50bffac5659923da3ac1ddcc82648
ROHIT-213 <rs2101396@gmail.com>
2026-05-10 00:09:05 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const headingElement = screen.getByText(/TODO LIST/i);
-  // const linkElement = screen.getByText(/learn react/i);
+  //const headingElement = screen.getByText(/TODO LIST/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
