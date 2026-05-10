8af196eb49aa557cab1b901e44ea9c7dc4b2daad
ROHIT-213 <rs2101396@gmail.com>
2026-05-10 09:15:37 +0530

    workspace

--- Appcode/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/TODO LIST/i);
-  // const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
