a2ed21c59ca212c91a9d10e33b156019107543cc
ROHIT-213 <rs2101396@gmail.com>
2026-05-13 23:26:02 +0530

    updated vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -184,8 +184,8 @@ resource "aws_vpc_security_group_ingress_rule" "eks_nodes_https" {
 
 resource "aws_vpc_security_group_egress_rule" "eks_nodes_all" {
   security_group_id = aws_security_group.eks_nodes.id
-  from_port         = 0
-  to_port           = 0
+  # from_port         = 0
+  # to_port           = 0
   ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
   description       = "Allow all outbound traffic"
